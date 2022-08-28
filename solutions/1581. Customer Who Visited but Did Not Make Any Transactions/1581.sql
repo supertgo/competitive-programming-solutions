b173456339a7df0d605709e7a4784ae91c1cda42
supertgo <thiagoroberto2003@gmail.com>
2022-08-27 21:06:54 -0300

    feat(solutions): :sparkles: 1581

---
+++ solutions/1581. Customer Who Visited but Did Not Make Any Transactions/1581.sql
@@ -0,0 +1,3 @@
+select customer_id, count(Visits.visit_id) as count_no_trans from Visits
+  left outer join Transactions on Visits.visit_id = Transactions.visit_id where Transactions.visit_id is null
+  group by Visits.customer_id;

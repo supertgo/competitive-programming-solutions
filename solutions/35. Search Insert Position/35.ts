442d112a07339172f6895f1301d3d4ce3dd55dc6
supertgo <thiagoroberto2003@gmail.com>
2022-09-01 16:03:04 -0300

    feat(solutions): add 35

---
+++ solutions/35. Search Insert Position/35.ts
@@ -0,0 +1,16 @@
+function searchInsert(nums: number[], target: number): number {
+  let start = 0;
+  let end = nums.length - 1;
+  
+  while(start <= end) {
+    const mid = Math.floor((start + end) / 2)
+    
+    if(nums[mid] === target) return mid;
+    
+    if(nums[mid] > target) end = mid - 1;
+    
+    if(nums[mid] < target) start = mid + 1;
+  }
+  
+  return start
+};
